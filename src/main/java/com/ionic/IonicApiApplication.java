6f85c3f06dfd91ea9258efd7ab649f53ff44bd93
Matheus <cavalcanti.smatheus@gmail.com>
2018-11-26 17:43:56 -0200

    Primeiro commit do projeto

---
+++ src/main/java/com/ionic/IonicApiApplication.java
@@ -0,0 +1,12 @@
+package com.ionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicApiApplication.class, args);
+	}
+}
