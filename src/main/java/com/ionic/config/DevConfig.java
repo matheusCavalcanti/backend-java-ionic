cdb554a92884049fafff4f5ba31e8f6b9f75b76c
Matheus <cavalcanti.smatheus@gmail.com>
2019-09-30 02:17:59 -0300

    Servico de envio de email com SMTP Google

--- src/main/java/com/ionic/config/DevConfig.java
+++ src/main/java/com/ionic/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ionic.service.DBService;
+import com.ionic.service.EmailService;
+import com.ionic.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
